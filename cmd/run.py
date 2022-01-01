3e309fbd78d87a28a865f9209a22c64c9c7f6fbe
kotokaze <62094392+kotokaze@users.noreply.github.com>
2022-01-02 02:07:23 +0900

    :sparkles: Use build artifacts

--- cmd/run.py
+++ cmd/run.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env python3.10
 
-"""
-This is the main entry point for the program.
-"""
-from pathlib import Path
-from sys import path
+"""run.py
 
-path.append(str(Path.joinpath(Path(__file__).parent.parent, "src")))
+    This is the main script.
+"""
 
-import comms.communicate
+import sys
 
 if __name__ == "__main__":
-    # comms/communicate内を実行
-    comms.communicate.do_communicate()
+    from racoon_ai.comms import communicate
+
+    sys.exit(communicate.do_communicate())
---
+++ racoon_ai/__init__.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3.10
+# pylint: disable=C0111
+
+__path__ = __import__("pkgutil").extend_path(__path__, __name__)
