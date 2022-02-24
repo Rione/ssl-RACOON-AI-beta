62597edd591230c0e0024ca2addaad3a01faf482
mukuyo <adgjmptw47777@gmail.com>
2022-02-24 11:50:23 +0900

    :memo:Add QLabel

--- racoon_ai/gui/main.py
+++ racoon_ai/gui/main.py
@@ -10,7 +10,7 @@
 import math
 
 from PyQt5.QtCore import QPoint, Qt
-from PyQt5.QtGui import QColor, QPainter
+from PyQt5.QtGui import QColor, QPainter, QPixmap
 from PyQt5.QtWidgets import QLabel, QWidget
 
 from racoon_ai.networks import VisionReceiver
@@ -27,7 +27,7 @@ class Gui(QWidget, QPainter):
 
     def __init__(self) -> None:
         super(Gui, self).__init__()
-        self.__geometry_width: int = 1300
+        self.__geometry_width: int = 960
         self.__geometry_height: int = 850
         self.__window_width: float
         self.__window_height: float
@@ -35,16 +35,18 @@ class Gui(QWidget, QPainter):
         self.__ball: SSL_DetectionBall
         self.__role = QLabel("<h1><i>Role</i></h1>", self)
         self.__at = QLabel("<h3><i>AT</i></h3>", self)
-        self.__of0 = QLabel("<h3><i>OF0</i></h3>", self)
-        self.__of1 = QLabel("<h3><i>OF1</i></h3>", self)
-        self.__of2 = QLabel("<h3><i>OF2</i></h3>", self)
-        self.__of3 = QLabel("<h3><i>OF3</i></h3>", self)
-        self.__of4 = QLabel("<h3><i>OF3</i></h3>", self)
-        self.__df0 = QLabel("<h3><i>DF0</i></h3>", self)
-        self.__df1 = QLabel("<h3><i>DF1</i></h3>", self)
-        self.__df2 = QLabel("<h3><i>DF2</i></h3>", self)
-        self.__df3 = QLabel("<h3><i>DF2</i></h3>", self)
-        self.__GK = QLabel("<h3><i>GK</i></h3>", self)
+        self.__ai = QLabel("<h3><i>RACOON-AI</i></h3>", self)
+        # self.__of0 = QLabel("<h3><i>OF0</i></h3>", self)
+        # self.__of1 = QLabel("<h3><i>OF1</i></h3>", self)
+        # self.__of2 = QLabel("<h3><i>OF2</i></h3>", self)
+        # self.__of3 = QLabel("<h3><i>OF3</i></h3>", self)
+        # self.__of4 = QLabel("<h3><i>OF3</i></h3>", self)
+        # self.__df0 = QLabel("<h3><i>DF0</i></h3>", self)
+        # self.__df1 = QLabel("<h3><i>DF1</i></h3>", self)
+        # self.__df2 = QLabel("<h3><i>DF2</i></h3>", self)
+        # self.__df3 = QLabel("<h3><i>DF2</i></h3>", self)
+        # self.__GK = QLabel("<h3><i>GK</i></h3>", self)
+
         self._initui()
 
     def vision_receive(self, vision: VisionReceiver) -> None:
@@ -61,9 +63,14 @@ class Gui(QWidget, QPainter):
         # print(self.__ball.x)
 
     def _initui(self) -> None:
-        self.resize(1300, 850)
+        self.resize(self.__geometry_width, self.__geometry_height)
         self.move(0, 0)
         self.setWindowTitle("RACOON-AI")
+        label = QLabel(self)
+        # label.setFixedSize(150, 150)
+        pixmap = QPixmap("game.jpg")
+        label.setPixmap(pixmap.scaledToHeight(32))
+        label.move(550, 10)
 
         self.show()
 
@@ -76,14 +83,14 @@ class Gui(QWidget, QPainter):
         """
         coat_width = event.size().width()
         coat_height = event.size().height()
-        self.__window_width = coat_width / 1300
-        self.__window_height = coat_height / 850
+        self.__window_width = coat_width / self.__geometry_width
+        self.__window_height = coat_height / self.__geometry_height
 
     def paintEvent(self, event) -> None:
         self.__ui = QPainter(self)
         self.__ui.setPen(QColor(Qt.white))
 
-        self.__ui.setBrush(QColor(Qt.green))
+        self.__ui.setBrush(QColor(Qt.gray))
         self._drawRect(20, 30, 520, 680)
         self._drawRect(60, 70, 440, 600)
         self._drawEllipse(280, 370, 30, 30)
@@ -105,11 +112,14 @@ class Gui(QWidget, QPainter):
         self.__ui.setPen(QColor(Qt.black))
         self.__ui.setBrush(QColor(Qt.white))
         self._draw_role()
+
+        # self._drawRect(550, 30, 120, 60)
         self.__ui.end()
 
     def _draw_role(self) -> None:
-        self.__role.move(int(600 * self.__window_width), int(740 * self.__window_height))
-        self.__at.move(int(600 * self.__window_width), int(780 * self.__window_height))
+        self.__role.move(int(20 * self.__window_width), int(740 * self.__window_height))
+        self.__at.move(int(20 * self.__window_width), int(780 * self.__window_height))
+        self.__ai.move(10, 5)
 
     def _draw_robots(self, color: str) -> None:
         if color == "blue":
@@ -124,7 +134,7 @@ class Gui(QWidget, QPainter):
                 self.__ui.setPen(QColor(Qt.black))
                 self.__ui.drawChord(
                     int(((robot.y * 0.05) + 280) * self.__window_width - 6),
-                    int(((robot.x * 0.05) + 370) * self.__window_width - 7),
+                    int(((robot.x * 0.05) + 370) * self.__window_height - 7),
                     14,
                     14,
                     int((math.degrees(robot.orientation) + 320) * 16),
