7ea10dc5471541bb7116c7a6a3458ce526e66dce
kotokaze <62094392+kotokaze@users.noreply.github.com>
2022-02-24 23:18:55 +0900

    :white_check_mark: Add Vector3f

--- racoon_ai/models/coordinate/__init__.py
+++ racoon_ai/models/coordinate/__init__.py
@@ -4,6 +4,7 @@
     This module contains:
         - Point
         - Pose
+        - Vector3f
 """
 
 from dataclasses import dataclass, field
@@ -14,9 +15,9 @@ class Point:
     """Point
 
     Attributes:
-        x (float): The x coordinate.
-        y (float): The y coordinate.
-        z (float): The z coordinate.
+        x (float): The x coordinate
+        y (float): The y coordinate
+        z (float): The z coordinate
     """
 
     x: float
@@ -27,22 +28,18 @@ class Point:
         return float((self.x ** 2 + self.y ** 2 + self.z ** 2) ** 0.5)
 
     def __add__(self, other: "Point") -> "Point":
-        """Add two points"""
         return Point(self.x + other.x, self.y + other.y, self.z + other.z)
 
     def __sub__(self, other: "Point") -> "Point":
-        """Subtract two points"""
         return Point(self.x - other.x, self.y - other.y, self.z - other.z)
 
     def __iadd__(self, other: "Point") -> "Point":
-        """Add two points"""
         self.x += other.x
         self.y += other.y
         self.z += other.z
         return self
 
     def __isub__(self, other: "Point") -> "Point":
-        """Subtract two points"""
         self.x -= other.x
         self.y -= other.y
         self.z -= other.z
@@ -64,3 +61,69 @@ class Pose:
     y: float
     theta: float = field(default=0)
     z: float = field(default=0)
+
+
+@dataclass()
+class Vector3f:
+    """Vector3f
+
+    Attributes:
+        x (float): The x value
+        y (float): The y value
+        z (float): The z value
+    """
+
+    x: float
+    y: float
+    z: float = field(default=0)
+
+    def __abs__(self) -> float:
+        return float((self.x ** 2 + self.y ** 2 + self.z ** 2) ** 0.5)
+
+    def __add__(self, other: "Vector3f") -> "Vector3f":
+        return Vector3f(self.x + other.x, self.y + other.y, self.z + other.z)
+
+    def __sub__(self, other: "Vector3f") -> "Vector3f":
+        return Vector3f(self.x - other.x, self.y - other.y, self.z - other.z)
+
+    def __iadd__(self, other: "Vector3f") -> "Vector3f":
+        self.x += other.x
+        self.y += other.y
+        self.z += other.z
+        return self
+
+    def __isub__(self, other: "Vector3f") -> "Vector3f":
+        self.x -= other.x
+        self.y -= other.y
+        self.z -= other.z
+        return self
+
+    def dot_prod(self, other: "Vector3f") -> float:
+        """dot_prod
+
+        Calculate the dot product
+
+        Args:
+            other (Vector3f): The other vector
+
+        Returns:
+            float: The dot product
+        """
+        return float(self.x * other.x + self.y * other.y + self.z * other.z)
+
+    def cross_prod(self, other: "Vector3f") -> "Vector3f":
+        """Cross product
+
+        Calculate the cross product
+
+        Args:
+            other (Vector3f): The other vector
+
+        Returns:
+            Vector3f: The cross product
+        """
+        return Vector3f(
+            self.y * other.z - self.z * other.y,
+            -self.x * other.z + self.z * other.x,
+            self.x * other.y - self.y * other.x,
+        )
