33c80f208604360306350de5f4252e9b7d37635f
TTRobot314 <69624612+TTRobot314@users.noreply.github.com>
2022-05-30 18:26:03 +0900

    Feature/keeper (#58)
    * 4/21
    
    * for_4/28
    
    * 1838
    
    * add_pid
    
    * fixed_pid
    
    * neo_pid
    
    * pid_common
    
    Co-authored-by: TTRobo314 <t.t.robot31415@gmail.com>

--- racoon_ai/__main__.py
+++ racoon_ai/__main__.py
@@ -5,11 +5,14 @@
 """
 from logging import INFO, Formatter, StreamHandler, getLogger, shutdown
 
+from .common.controls import Controls
 from .models.robot import SimCommands
 from .networks.receiver import MWReceiver
 from .networks.sender import CommandSender
-from .strategy.offense import Offense
-from .strategy.role import Role
+
+# from .strategy.offense import Offense
+# from .strategy.role import Role
+from .strategy.goal_keeper import Keeper
 
 
 def main() -> None:
@@ -40,13 +43,17 @@ def main() -> None:
     is_team_yellow: bool = False
 
     try:
+
         observer = MWReceiver(host="127.0.0.1", port=30011)
+        # role = Role(observer)
 
-        role = Role(observer)
+        controls = Controls(observer)
 
-        offense = Offense(observer, role)
+        # offense = Offense(observer)
 
-        sender = CommandSender(is_real, online_ids, host="localhost")
+        keeper = Keeper(observer, controls)
+
+        sender = CommandSender(is_real, online_ids, host="localhost", port=20025)
 
         logger.info("Roop started")
 
@@ -55,10 +62,12 @@ def main() -> None:
             sim_cmds = SimCommands(is_team_yellow)
 
             observer.main()
-            role.main()
-            offense.main()
+            # role.main()
+            # offense.main()
+            keeper.main()
 
-            sim_cmds.robot_commands += offense.send_cmds
+            # sim_cmds.robot_commands += offense.send_cmds
+            sim_cmds.robot_commands += keeper.send_cmds
             sender.send(sim_cmds)
 
     except KeyboardInterrupt:
