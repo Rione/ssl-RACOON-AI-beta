1b7de048b926ad58e6ccbbd6fffccc01b540174e
kotokaze <62094392+kotokaze@users.noreply.github.com>
2022-06-18 12:49:43 +0900

    :white_check_mark: Include the QApplication in Gui

--- racoon_ai/gui/view.py
+++ racoon_ai/gui/view.py
@@ -1,35 +1,40 @@
 #!/usr/bin/env python3.10
 # flake8: ignore-errors
 # pylint: disable-all
-# type: ignore
+
 """view.py
 
     This module is for the Gui class.
 """
 
-from racoon_ai.gui.background import Back
-from racoon_ai.gui.chart import Chart
-from racoon_ai.gui.game_control import Game
-from racoon_ai.gui.main import Main
-from racoon_ai.gui.robot import Robot
-from racoon_ai.gui.vision import Vision
+from PyQt6.QtWidgets import QApplication
+
 from racoon_ai.networks.receiver import MWReceiver
 from racoon_ai.strategy.role import Role
 
+from .background import Back
+from .chart import Chart
+from .game_control import Game
+from .main import Main
+from .robot import Robot
+from .vision import Vision
+
 
 class Gui:
-    def __init__(self, is_gui_view: bool, observer: MWReceiver, role: Role):
+    def __init__(self, argv: list[str], is_gui_view: bool, observer: MWReceiver, role: Role):
+        self.__app = QApplication(argv)
         self.__is_gui_view = is_gui_view
         if self.__is_gui_view:
             self.__main = Main(observer, role)
             self.__chart = Chart(self.__main, observer)
-            _ = Vision(self.__main)
-            _ = Robot(self.__main)
-            _ = Back(self.__main)
-            _ = Game(self.__main)
+            Vision(self.__main)
+            Robot(self.__main)
+            Back(self.__main)
+            Game(self.__main)
 
             self.__main.show()
 
     def update(self) -> None:
         if self.__is_gui_view:
             self.__main.update()
+            self.__app.processEvents()
