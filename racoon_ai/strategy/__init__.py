29b04bbe0d31054d5c4216e1f4b3b238032452a2
Kotokaze <62094392+kotokaze@users.noreply.github.com>
2022-06-21 16:54:04 +0900

    :warning: Fix config file (#102)
    * :warning: Remove config
    
    * :fire: Cleanup main

--- racoon_ai/strategy/__init__.py
+++ racoon_ai/strategy/__init__.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python 3.10
 # pylint: disable=C0114
 
+from .defense import Defense
 from .goal_keeper import Keeper
 from .offense import Offense
 from .role import Role
+from .subrole import SubRole
 
 __all__ = [
+    "Defense",
     "Keeper",
     "Offense",
     "Role",
+    "SubRole",
 ]
