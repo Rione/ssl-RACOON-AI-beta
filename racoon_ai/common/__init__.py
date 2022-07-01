95f20816c555c3dc1ffa85dacf89bc58989cd94e
kotokaze <62094392+kotokaze@users.noreply.github.com>
2022-07-02 03:22:24 +0900

    :warning: Resolve import error

--- racoon_ai/common/__init__.py
+++ racoon_ai/common/__init__.py
@@ -1,42 +1,8 @@
 #!/usr/bin/env python3.10
 # pylint: disable=C0114
 
-from math import cos, sin
-from typing import TypeAlias
-
-from racoon_ai.models.coordinate import Pose
-from racoon_ai.models.robot import Robot, RobotCommand
-
 from .math_utils import MathUtils
-
-MAX_SPEED: float = 1000.0
-CLOSE_BALL: float = 150.0
-MU: TypeAlias = MathUtils
-
-
-def move2pose(robot: Robot, dist: Pose) -> RobotCommand:
-    """move2pose
-
-    Returns:
-        RobotCommand: move motion value
-    """
-    command = RobotCommand(robot.robot_id)
-    rotation = MU.radian_reduce(dist.theta, robot.theta)
-    radian_target_robot = MU.radian_reduce(MU.radian(dist, robot), robot.theta)
-    distance_target_robot = MU.distance(dist, robot)
-
-    speed = min(distance_target_robot / 100.0, MAX_SPEED)
-    command.vel_fwd = cos(radian_target_robot) * speed
-    command.vel_sway = sin(radian_target_robot) * speed
-    command.vel_angular = radian_target_robot - rotation
-    command.kickpow = 0.0
-    command.dribble_pow = 0.0
-    return command
-
 
 __all__ = [
     "MathUtils",
-    "CLOSE_BALL",
-    "MAX_SPEED",
-    "move2pose",
 ]
--- racoon_ai/movement/__init__.py
+++ racoon_ai/movement/__init__.py
@@ -6,6 +6,7 @@ from logging import Logger
 
 from racoon_ai.observer import Observer
 
+from .basics import move2pose
 from .controls import Controls
 
 
@@ -31,4 +32,5 @@ def create_controls(config: ConfigParser, logger: Logger, observer: Observer) ->
 __all__ = [
     "Controls",
     "create_controls",
+    "move2pose",
 ]
