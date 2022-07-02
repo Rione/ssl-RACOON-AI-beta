dc3eb4f924d199ac8f0b9c997075996f276f5731
kotokaze <62094392+kotokaze@users.noreply.github.com>
2022-07-02 14:54:24 +0900

    :sparkles: Add StrategyBase class

--- racoon_ai/strategy/defense.py
+++ racoon_ai/strategy/defense.py
@@ -2,7 +2,8 @@
 
 """defense.py
 
-    This module is for the defense class.
+    This module contains:
+        - Defense
 """
 
 from logging import getLogger
@@ -14,30 +15,35 @@ from numpy import sign
 from racoon_ai.common.math_utils import MathUtils as MU
 from racoon_ai.models.coordinate import Pose
 from racoon_ai.models.robot import Robot, RobotCommand
-from racoon_ai.movement.controls import Controls
+from racoon_ai.movement import Controls
 from racoon_ai.observer import Observer
-from racoon_ai.strategy.role import Role
-from racoon_ai.strategy.subrole import SubRole
+
+from .base import StrategyBase
+from .role import Role
+from .subrole import SubRole
 
 
-class Defense:
-    """Defense
+class Defense(StrategyBase):
+    """Defense(StrategyBase)
+
     Args:
-        controls (Controls): Controls instance
         observer (Observer): Observer instance
+        role (Role): Role instance
+        subrole (SubRole): SubRole instance
+        controls (Controls): Controls instance
 
     Attributes:
         send_cmds (list[RobotCommand]): RobotCommand list.
     """
 
     def __init__(self, observer: Observer, role: Role, subrole: SubRole, controls: Controls) -> None:
+        super().__init__(observer, controls, role)
+
         self.__logger = getLogger(__name__)
         self.__logger.info("Initializing...")
-        self.__observer = observer
-        self.__role = role
-        self.__subrole = subrole
-        self.__controls = controls
-        self.__send_cmds: list[RobotCommand]
+
+        self.__subrole: SubRole = subrole
+
         self.__enemy_offense: list[int] = []
         self.__defense_quantity: int = 0
         self.__max_robot_radius: float = 90
@@ -46,21 +52,11 @@ class Defense:
         self.__enemy_attacker: int = -1
         self.__count: int = 0
 
-    @property
-    def send_cmds(self) -> list[RobotCommand]:
-        """send_cmdsDefence
-
-        Returns:
-            list[RobotCommand]: send_cmds
-        """
-        return self.__send_cmds
-
     def main(self) -> None:
         """main"""
-
-        self.__send_cmds = []
-        self.__defense_quantity = self.__role.get_defense_quantity
-        self.__enemy_quantity = self.__observer.num_of_enemy_robots
+        self.send_cmds = []
+        self.__defense_quantity = self.role.get_defense_quantity
+        self.__enemy_quantity = self.observer.num_of_enemy_robots
         self.__diff_defense_enemy_quantity = self.__defense_quantity - (self.__enemy_quantity - 1)
         self.__enemy_attacker = self.__subrole.enemy_attacker_id
         self.__count = 0
@@ -76,17 +72,17 @@ class Defense:
 
         if self.__enemy_quantity <= 1:
             for i in range(self.__defense_quantity):
-                bot = self.__observer.get_our_by_id(self.__role.get_defense_id(i))
+                bot = self.observer.get_our_by_id(self.role.get_defense_id(i))
                 if bot:
                     cmd = self.__default_position(bot, i)
-                    self.__send_cmds.append(cmd)
+                    self.send_cmds.append(cmd)
         else:
             for i in range(self.__defense_quantity):
-                bot = self.__observer.get_our_by_id(self.__role.get_defense_id(i))
-                ene = self.__observer.get_enemy_by_id(self.__enemy_offense[i])
+                bot = self.observer.get_our_by_id(self.role.get_defense_id(i))
+                ene = self.observer.get_enemy_by_id(self.__enemy_offense[i])
                 if bot and ene:
                     cmd = self.__keep_penalty_area(bot, ene)
-                    self.__send_cmds.append(cmd)
+                    self.send_cmds.append(cmd)
 
     def __enemy_offense_decide(self) -> None:
         """enemy_offense_decide"""
@@ -97,10 +93,10 @@ class Defense:
         enemy_offense = [
             (
                 enemy.robot_id,
-                MU.distance(enemy, self.__observer.geometry.goal),
-                MU.radian(enemy, self.__observer.geometry.goal),
+                MU.distance(enemy, self.observer.geometry.goal),
+                MU.radian(enemy, self.observer.geometry.goal),
             )
-            for enemy in self.__observer.enemy_robots
+            for enemy in self.observer.enemy_robots
             if enemy.is_visible is True
         ]
 
@@ -108,9 +104,9 @@ class Defense:
             for _ in range(self.__diff_defense_enemy_quantity):
                 enemy_offense.append(
                     (
-                        self.__observer.enemy_robots[self.__enemy_attacker].robot_id,
-                        MU.distance(self.__observer.enemy_robots[self.__enemy_attacker], self.__observer.geometry.goal),
-                        MU.radian(self.__observer.enemy_robots[self.__enemy_attacker], self.__observer.geometry.goal),
+                        self.observer.enemy_robots[self.__enemy_attacker].robot_id,
+                        MU.distance(self.observer.enemy_robots[self.__enemy_attacker], self.observer.geometry.goal),
+                        MU.radian(self.observer.enemy_robots[self.__enemy_attacker], self.observer.geometry.goal),
                     )
                 )
 
@@ -122,27 +118,27 @@ class Defense:
 
     def __keep_penalty_area(self, robot: Robot, enemy: Robot) -> RobotCommand:
         """keep_penalty_area"""
-        radian_enemy_goal = MU.radian(enemy, self.__observer.geometry.goal)
+        radian_enemy_goal = MU.radian(enemy, self.observer.geometry.goal)
 
         if abs(radian_enemy_goal) >= MU.PI / 2:
             radian_enemy_goal = (sign(radian_enemy_goal) * MU.PI) / 2
 
         if abs(radian_enemy_goal) < MU.PI / 4:
             target_pose = Pose(
-                (self.__observer.geometry.goal.x + self.__observer.geometry.goal_width + self.__max_robot_radius),
-                ((self.__observer.geometry.goal_width + self.__max_robot_radius) * tan(radian_enemy_goal)),
+                (self.observer.geometry.goal.x + self.observer.geometry.goal_width + self.__max_robot_radius),
+                ((self.observer.geometry.goal_width + self.__max_robot_radius) * tan(radian_enemy_goal)),
                 radian_enemy_goal,
             )
 
         else:
             target_pose = Pose(
                 (
-                    self.__observer.geometry.goal.x
-                    + (self.__observer.geometry.goal.y + self.__observer.geometry.goal_width + self.__max_robot_radius)
+                    self.observer.geometry.goal.x
+                    + (self.observer.geometry.goal.y + self.observer.geometry.goal_width + self.__max_robot_radius)
                     / tan(radian_enemy_goal)
                     * sign(radian_enemy_goal)
                 ),
-                (self.__observer.geometry.goal.y + (self.__observer.geometry.goal_width + self.__max_robot_radius))
+                (self.observer.geometry.goal.y + (self.observer.geometry.goal_width + self.__max_robot_radius))
                 * sign(radian_enemy_goal),
                 radian_enemy_goal,
             )
@@ -156,7 +152,7 @@ class Defense:
                 ) * sign(radian_enemy_goal)
             self.__count += 1
 
-        command: RobotCommand = self.__controls.pid(target_pose, robot)
+        command: RobotCommand = self.controls.pid(target_pose, robot)
         command.dribble_pow = 0
         command.kickpow = 0
         return command
@@ -164,22 +160,22 @@ class Defense:
     def __default_position(self, robot: Robot, i: int) -> RobotCommand:
         """keep_penalty_area"""
 
+        target_pose: Pose
         if self.__defense_quantity == 1:
             target_pose = Pose(
-                (self.__observer.geometry.goal.x + self.__observer.geometry.goal_width + self.__max_robot_radius),
-                self.__observer.geometry.goal_y,
+                (self.observer.geometry.goal.x + self.observer.geometry.goal_width + self.__max_robot_radius),
+                self.observer.geometry.goal_y,
                 0,
             )
 
         else:
             target_pose = Pose(
-                (self.__observer.geometry.goal.x + self.__observer.geometry.goal_width + self.__max_robot_radius),
+                (self.observer.geometry.goal.x + self.observer.geometry.goal_width + self.__max_robot_radius),
                 (
-                    self.__observer.geometry.goal_width
-                    - i * (self.__observer.geometry.goal_width * 2 / (self.__defense_quantity - 1))
+                    self.observer.geometry.goal_width
+                    - i * (self.observer.geometry.goal_width * 2 / (self.__defense_quantity - 1))
                 ),
                 0,
             )
 
-        command: RobotCommand = self.__controls.pid(target_pose, robot)
-        return command
+        return self.controls.pid(target_pose, robot)
