c3f7c0ff92ef317ab5802e6196a283acd9aa5256
Kotokaze <62094392+kotokaze@users.noreply.github.com>
2022-07-03 11:52:45 +0900

    :warning: Use compiled stub files (#129)
    * :warning: Use compiled stub files
    
    * :bookmark: Upgrade to 0.5.3

--- racoon_ai/models/referee/__init__.py
+++ racoon_ai/models/referee/__init__.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3.10
 # pylint: disable=C0114
 
-from .referee import Referee
+from .referee import REF_COMMAND, REF_STAGE, Referee
 
 __all__ = [
+    "REF_COMMAND",
+    "REF_STAGE",
     "Referee",
 ]
