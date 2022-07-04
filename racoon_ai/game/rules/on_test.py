bad3234713d2e81733df02bac0c7c412f703662f
Kotokaze <62094392+kotokaze@users.noreply.github.com>
2022-07-04 15:33:47 +0900

    :sparkles: Add ball move track (#136)

--- racoon_ai/game/rules/on_test.py
+++ racoon_ai/game/rules/on_test.py
@@ -9,20 +9,20 @@
 from logging import Logger
 
 from racoon_ai.models.robot import RobotCommand
-from racoon_ai.strategy import Defense, Keeper, Offense
+from racoon_ai.strategy import BallPlacement
 
-from .on_normal_start import on_default_cbf
+from .on_placement import on_placement_our_cbf
 
 
-def test_cbf(logger: Logger, strategies: tuple[Defense, Keeper, Offense]) -> list[RobotCommand]:
+def test_cbf(logger: Logger, args: tuple[BallPlacement]) -> list[RobotCommand]:
     """on_default_cbf
 
     This function is called when the game is running normally.
 
     Args:
         logger (Logger): Logger instance.
-        strategies: tuple[Defense, Keeper, Offense]
+        args: tuple[BallPlacement]
     """
-    send_cmds: list[RobotCommand] = on_default_cbf(logger, strategies)
+    send_cmds: list[RobotCommand] = on_placement_our_cbf(logger, args)
     logger.debug(send_cmds)
     return send_cmds
